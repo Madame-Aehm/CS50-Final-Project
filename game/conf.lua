05adaf67ee65a1f8a97ed630d2c6713c1570a795
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-09-12 17:53:03 +0200

    tutorial steps =< 10

--- game/conf.lua
+++ game/conf.lua
@@ -20,14 +20,14 @@ function love.conf(t)
 	t.audio.mic             = false
 	t.audio.mixwithsystem   = true
 
-	t.window.title          = "Untitled"
+	t.window.title          = "Whodunnit?"
 	t.window.icon           = nil
 	t.window.width          = 800
 	t.window.height         = 600
 	t.window.borderless     = false
 	t.window.resizable      = false
-	t.window.minwidth       = 1
-	t.window.minheight      = 1
+	t.window.minwidth       = 800
+	t.window.minheight      = 600
 	t.window.fullscreen     = false
 	t.window.fullscreentype = "desktop"
 	t.window.vsync          = 1
--- game/main.lua
+++ game/main.lua
@@ -1,9 +1,22 @@
 function love.load()
+	-- require("sprite")
+	-- windowWidth = love.graphics.getWidth()
+	-- windowHeight = love.graphics.getHeight()
+
+	tick = require("lib/tick")
+	drawRect = false
+	tick.delay(function() drawRect = true end, 2)
 end
 
-function love.update()
-	print("Hello!")
-end
 
 function love.draw()
+	-- love.graphics.circle("fill", sprite.posX, sprite.posY, sprite.size)
+	-- if drawRect then
+		love.graphics.rectangle("line", 100, 100, 100, 100)
+	-- end
 end
+
+
+-- function love.update(dt)
+-- 	spriteControl(dt)
+-- end
