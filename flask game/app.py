8cfc19edaed5d771112812d64256237905028307
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-09-24 18:18:46 +0200

    flask env set up + first few images

--- flask game/app.py
+++ flask game/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+# python -m flask --app app run
+# python -m flask --app app run --debug
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("layout.html")
+
+@app.route("/police-station")
+def police_station():
+    return render_template("police_station.html")
