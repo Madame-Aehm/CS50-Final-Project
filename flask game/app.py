4714f69c67522bd7b6dfae530a48a224c869c5ec
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-09-26 17:40:46 +0200

    images + improve flow

--- flask game/app.py
+++ flask game/app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("layout.html")
+    return render_template("index.html")
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
 
 @app.route("/police-station")
 def police_station():
