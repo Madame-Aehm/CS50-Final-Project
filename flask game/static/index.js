5c41ed3ed9c9d8dc9df8fccf9154e311754fbe76
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-10-01 15:38:03 +0200

    clue modal

--- flask game/static/index.js
+++ flask game/static/index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const addEventControllers = () => {
+    const clueControl = document.getElementById("clue-control");
+    clueControl.addEventListener("click", () => {
+        const clueModel = document.getElementById("clue-model");
+        clueModel.classList.remove("hidden");
+        clueControl.classList.add("hidden");
+    });
+
+    const closeModel = document.getElementById("close-model");
+    closeModel.addEventListener("click", () => {
+        const clueModel = document.getElementById("clue-model");
+        clueModel.classList.add("hidden");
+        clueControl.classList.remove("hidden");
+    });
+}
+
+addEventControllers();
