45bdf90a9f14244b88d8b6969e9e605b47bbf731
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-10-31 16:33:48 +0100

    populate supabase

--- flask game/app.py
+++ flask game/app.py
@@ -1,9 +1,17 @@
 # python -m flask --app app run
 # python -m flask --app app run --debug
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 def hello_world():
@@ -13,6 +21,17 @@ def hello_world():
 def map():
     return render_template("map.html")
 
-@app.route("/police-station")
+@app.route("/police-station", methods=["GET", "POST"])
 def police_station():
-    return render_template("police_station.html")
+    if request.method == "GET":
+        return render_template("police_station.html")
+    if request.method == "POST":
+        if (request.form.get("id") == "reports"):
+            day = request.form.get("day")
+            month = request.form.get("month")
+            year = request.form.get("year")
+            street = request.form.get("street")
+            if day and month and year and street:
+                print("all values present")
+            else: redirect("/police_station.html", report_error="All values must be specified")
+        return redirect("/police-station")
