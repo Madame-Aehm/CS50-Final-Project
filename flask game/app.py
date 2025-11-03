23e2fad91cb65383213ff24a71956588f11fd808
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-11-03 16:24:44 +0100

    error page + supabase connection

--- flask game/app.py
+++ flask game/app.py
@@ -2,8 +2,15 @@
 # python -m flask --app app run --debug
 
 from flask import Flask, render_template, request, redirect
+from utils import validate_values
+from dotenv import load_dotenv
+import os
+from supabase import create_client
+
+load_dotenv()
 
 app = Flask(__name__)
+
 
 @app.after_request
 def after_request(response):
@@ -13,18 +20,27 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("error.html", message=e)
+
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 
 @app.route("/map")
 def map():
     return render_template("map.html")
 
+
 @app.route("/police-station", methods=["GET", "POST"])
 def police_station():
     if request.method == "GET":
         return render_template("police_station.html")
+    
     if request.method == "POST":
         if (request.form.get("id") == "reports"):
             day = request.form.get("day")
@@ -32,6 +48,26 @@ def police_station():
             year = request.form.get("year")
             street = request.form.get("street")
             if day and month and year and street:
-                print("all values present")
-            else: redirect("/police_station.html", report_error="All values must be specified")
+                supabase = create_client(
+                    os.getenv("SUPABASE_URL"), 
+                    os.getenv("SUPABASE_KEY"))
+                response = (supabase.table("crime_scene_reports")
+                    .select()
+                    .eq("street", street)  
+                    .eq("day", int(day))
+                    .eq("month", int(month))
+                    .eq("year", int(year))
+                    .execute()
+                )
+            else: 
+                message = f"""
+                All values are required.
+                {validate_values([
+                    ("day", day), 
+                    ("month", month), 
+                    ("year", year), 
+                    ("street", street)])}
+                """
+                return render_template("error.html", message=message)
         return redirect("/police-station")
+    
---
+++ flask game/utils.py
@@ -0,0 +1,11 @@
+def validate_values(values):
+    print("VALUES", values)
+    missing_values = []
+    for value in values:
+        if bool(value[1]) == False:
+            missing_values.append(value[0])
+    print("MESSAGE AFTER LOOP", missing_values)
+    if len(missing_values) > 0:
+        missing_values[-1] = f"and {missing_values[-1]}"
+        return f"Missing values: {", ".join(missing_values)}."
+    return ""
