3df3ef32532a091e2f1d0c935ab8d1224bafd404
Madame-Aehm <emilyrachelstickler@gmail.com>
2026-07-27 19:11:10 +0200

    gunicorn port

--- flask game/app.py
+++ flask game/app.py
@@ -15,6 +15,11 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 csrf = CSRFProtect(app)
 
